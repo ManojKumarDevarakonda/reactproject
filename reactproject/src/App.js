224ebd4d401cb8b01d4eea16e32ce15b4eec58b3
ManojKumarDevarakonda <manojdevarakonda786@gmail.com>
2023-07-20 10:43:13 +0530

    create New Component

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -1,10 +1,14 @@
- import './App.css';
+import './App.css';
+import Example from './example';
 
+
+ 
 function App() {
   return (
     <div className="App">
      <div>Git Example With React</div>
-    </div>
+     <Example/>
+      </div>
   );
 }
 
